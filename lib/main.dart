aaeec83a78432025124e5e3c45b75bf1562c7636
bagusadlan <bagusadlann@gmail.com>
2023-10-18 16:17:04 +0700

    feat(main): change project title

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:imdbcloning/detail_screen.dart';
 
 import 'main_screen.dart';
 
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IMDb Cloning',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
